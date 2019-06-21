726770b5bd88e8b9029e0f89156bf6a8f01bdf8c
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-06-22 02:35:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -139,5 +139,40 @@
 #         return x,y
 # D=Dice()
 # print(D.roll())
+from pathlib import Path
+#
+# path=Path("ecommerce")
+#
+# print(path.exists())
+# path1=Path("emails")
+# path1.mkdir()
+# path1.rmdir()
+# path=Path()
+# for file in path.glob('*.py'):
+#     print(file)
+#* *.*
 
+import openpyxl as xl
+from openpyxl.chart import BarChart,Reference
+wb= xl.load_workbook('transactions.xlsx')
+sheet= wb['Sheet1']
+cell=sheet['a1']
+cell=sheet.cell(1,1)
+print(cell.value)
+print(sheet.max_row)
 
+for row in  range(2,sheet.max_row+1):
+     cell= sheet.cell(row,3)
+     new=cell.value * 0.9
+     corrected_price_cell=sheet.cell(row,4)
+     corrected_price_cell.value=new
+wb.save('transactions2.xlsx')
+
+values= Reference(sheet,
+          min_row=2,max_row=sheet.max_row,
+          min_col=4,
+          max_col=4
+          )
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart,'e2')
