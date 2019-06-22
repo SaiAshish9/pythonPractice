18a1b9421a2b40e3f8e9e2fbf22e17c6b906fad8
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-06-22 13:23:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -163,16 +163,18 @@ print(sheet.max_row)
 
 for row in  range(2,sheet.max_row+1):
      cell= sheet.cell(row,3)
-     new=cell.value * 0.9
+     print(cell.value)
      corrected_price_cell=sheet.cell(row,4)
-     corrected_price_cell.value=new
+     corrected_price_cell.value=cell.value * 0.9
+#
 wb.save('transactions2.xlsx')
-
-values= Reference(sheet,
-          min_row=2,max_row=sheet.max_row,
+values = Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
           min_col=4,
           max_col=4
           )
 chart = BarChart()
 chart.add_data(values)
-sheet.add_chart(chart,'e2')
+sheet.add_chart(chart,'E2')
+wb.save('transactions2.xlsx')
